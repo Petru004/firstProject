9b8b460a6f16519da2d1d742142f667f59bb140f
Petru <nicupetru@gmail.com>
2022-05-12 14:43:15 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,75 @@
+// Переменные и строгий режим
+"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 3;
+
+// number = 10;
+// console.log(number + leftBorderWidth);
+
+
+// Класификация типов данных
+
+// let number = 7.4;     // Int
+// console.log(4/number);
+
+// const person = 'Kolea'   // String
+// console.log(person)
+
+// const bool = false    // Boolean
+
+// console.log(something)  // null
+
+
+// const obj = {
+//     name: 'Jony',
+//     age: 25,
+//     isMarried: false,
+// };
+// console.log(obj.age);
+
+// let arr = ['plum.png' , 'ORANGE.jpg' , 'apple.bmp']
+// console.log(arr[2])
+
+
+
+// Простое общение с пользователем
+
+// const result = confirm('Are you here?');
+// console.log(result)
+
+// const answer = +prompt('Aveti 18 ani' , '18')
+// console.log(answer + 5)
+
+// const answers = []
+
+// answers[0] = prompt('Cum va cheama?' , '')
+// answers[1] = prompt('Cati ani ai?' , '')
+// answers[2] = prompt('Cum e familia ta?' , '')
+
+// document.write(answers)
+
+// console.log(typeof(answers))
+
+
+
+// Интерполяция
+
+// const main = 'Ivan'
+// const age = 17
+// const text = (`Pe el il cheama ${main} si el are ${age} ani`)
+// console.log(text)
+
+
+// Операторы в  JavaScript
+
+// console.log(4 + +'7')
+
+// let incr = 70,
+//     decr = 20;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(4-2 === '2')
+// && - and ; || - or ; ! - not;
